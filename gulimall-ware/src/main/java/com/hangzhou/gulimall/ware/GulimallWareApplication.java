b04c594fbaa5172eb6268ba1e121b9662669d206
lch19970210 <1068333515@qq.com>
2021-02-10 15:01:47 +0800

    添加dao层扫描注解;

--- gulimall-ware/src/main/java/com/hangzhou/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/hangzhou/gulimall/ware/GulimallWareApplication.java
@@ -1,9 +1,11 @@
 package com.hangzhou.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hangzhou.gulimall.ware.dao")
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
