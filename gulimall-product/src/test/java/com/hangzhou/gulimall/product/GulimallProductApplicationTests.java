9891f10307316d1427d5005f627a765019a3b98f
lch19970210 <1068333515@qq.com>
2021-02-21 17:12:07 +0800

    整合oss上传功能

--- gulimall-product/src/test/java/com/hangzhou/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/hangzhou/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,8 @@
 package com.hangzhou.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.hangzhou.gulimall.product.entity.BrandEntity;
 import com.hangzhou.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
@@ -8,12 +11,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    void testUpload() throws FileNotFoundException {
+//        // Endpoint以杭州为例，其它Region请按实际情况填写。
+//        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
+//        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
+//        String accessKeyId = "LTAI4G518dGevkoShoyoH5RT";
+//        String accessKeySecret = "gqBv7tw5iOvrfsXWmlkb0As9hmCb9W";
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        String bucketName = "gulimall-202102";
+        // <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
+        String objectName = "mbp16touch.jpeg";
+
+        // 上传文件到指定的存储空间（bucketName）并将其保存为指定的文件名称（objectName）。
+        InputStream inputStream = new FileInputStream("/Users/faye/Tools/gulimall/oss/mbp16touch.jpeg");
+        ossClient.putObject(bucketName, objectName, inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("success");
+    }
 
     @Test
     void contextLoads() {
