daddcc414683bd11b850967b3fa64fae7d2c100c
lch19970210 <1068333515@qq.com>
2021-02-23 09:21:11 +0800

    新增JSR303表单校验

--- gulimall-product/src/main/java/com/hangzhou/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/hangzhou/gulimall/product/controller/BrandController.java
@@ -1,9 +1,11 @@
 package com.hangzhou.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import com.hangzhou.gulimall.product.service.BrandService;
 import com.hangzhou.common.utils.PageUtils;
 import com.hangzhou.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -58,8 +61,18 @@ public class BrandController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@Valid @RequestBody BrandEntity brand, BindingResult result){
+        if(result.hasErrors()){
+            HashMap map = new HashMap();
+            result.getFieldErrors().forEach((item) ->{
+                String message = item.getDefaultMessage();
+                String field = item.getField();
+                map.put(field,message);
+            });
+            return R.error(400,"提交的数据不合法").put("data",map);
+        }else {
+            brandService.save(brand);
+        }
 
         return R.ok();
     }
--- gulimall-product/src/main/java/com/hangzhou/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/hangzhou/gulimall/product/entity/BrandEntity.java
@@ -8,6 +8,11 @@ import java.util.Date;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 /**
  * 品牌
@@ -29,10 +34,12 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌名
 	 */
+	@NotBlank(message = "品牌名必须非空")
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@URL(message = "logo必须是合法的URL地址")
 	private String logo;
 	/**
 	 * 介绍
@@ -45,10 +52,12 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 检索首字母
 	 */
+	@Pattern(regexp = "/^[a-zA-Z]$/",message = "检索首字母必须是一个字母")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
+	@Min(value = 0,message = "排序必须大于等于0")
 	private Integer sort;
 
 }
